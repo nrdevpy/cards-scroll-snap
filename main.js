36280ee8e235e0d945493106bf7685035fc8285c
nrdev <nelson_rojas_janda@hotmail.com>
2023-01-07 13:04:26 +0100

    Add logical active bullet

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        entry.target.classList.toggle('show', entry.isIntersecting)
-    })
-},{
-    threshold: .75,
-}
-)
 const cards = document.querySelectorAll('.card')
 const bullets = document.querySelector('.bullets')
 let bulletsHtml = '';
 
-cards.forEach(card =>{
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting)
+
+        let count = 0;
+
+        if (entry.isIntersecting) {
+            cards.forEach(card => {
+                if (card.className.includes('show')) {
+                    const activeBullet = bullets.children[count];
+                    activeBullet.classList.toggle('active')
+                }
+                count += 1;
+            })
+        }
+    })
+}, {
+    threshold: 0,
+})
+
+cards.forEach(card => {
     observer.observe(card)
     bulletsHtml += `
         <li class="bullet"></li>
