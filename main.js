39eac6fbc845db49cfdf20c982a1ca925c5bc900
nrdev <nelson_rojas_janda@hotmail.com>
2023-01-07 11:27:10 +0100

    Make cards horizontally scrollable

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.toggle('show', entry.isIntersecting)
     })
-})
-
+},{
+    threshold: .75,
+}
+)
 const cards = document.querySelectorAll('.card')
 
 cards.forEach(card =>{
